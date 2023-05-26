9f481aed52952edd34038e75d93fa0e188fee89e
junvary <junvary@foxmail.com>
2023-05-26 09:37:19 +0800

    init

--- dir/dir.go
+++ dir/dir.go
@@ -1,1 +1,23 @@
 package dir
+
+import (
+	"os"
+	"strings"
+)
+
+func GetAllFilesInDirWithSuffix(dirPath string, suffix string, fileList []string) ([]string, error) {
+	files, err := os.ReadDir(dirPath)
+	for _, file := range files {
+		if file.IsDir() {
+			fileList, err = GetAllFilesInDirWithSuffix(dirPath+"/"+file.Name(), suffix, fileList)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			if strings.HasSuffix(file.Name(), suffix) {
+				fileList = append(fileList, dirPath+"/"+file.Name())
+			}
+		}
+	}
+	return fileList, err
+}
